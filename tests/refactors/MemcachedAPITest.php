71bc4d82155816f80a58f62f47590cb295df7176
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-04-18 19:25:58 -0400

    Add another unit test

--- tests/refactors/MemcachedAPITest.php
+++ tests/refactors/MemcachedAPITest.php
@@ -26,4 +26,30 @@ final class MemcachedAPITest extends TestCase
     "message": "tbd -list all commands here"
 }', $results);
     }
+
+    #[Test] public function it_calls_memcached_api_and_responds()
+    {
+        // assemble
+        $argv = [];
+        $argc = 0;
+        $get = [
+            'stats' => '',
+        ];
+        $session = [];
+
+        // act
+        $memcached = new MemcachedAPI($argv, $argc, $get, $session);
+        $results = $memcached->memcached_api();
+
+        // assert
+        $results_array = json_decode($results, 1);
+        $this->assertArrayHasKey('command', $results_array);
+        $this->assertIsArray($results_array);
+        $this->assertIsArray($results_array['command']);
+        $this->assertArrayHasKey('query', $results_array['command']);
+        $this->assertArrayHasKey('value', $results_array['command']);
+        $this->assertArrayHasKey('hostname', $results_array['command']);
+        $this->assertArrayHasKey('stats', $results_array);
+        $this->assertIsArray($results_array['stats']);
+    }
 }
