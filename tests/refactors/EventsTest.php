83a3522948aa43360e5289e5fd00120d9a95746c
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-05-17 12:16:37 -0400

    Add new PHPUnit test stubs for class methods

---
+++ tests/refactors/EventsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace refactors;
+
+use mattjgagnon\RefactoringPhp\refactors\Events;
+use PHPUnit\Framework\TestCase;
+
+class EventsTest extends TestCase
+{
+
+    public function testEvents_delete()
+    {
+
+    }
+
+    public function testEvents_update()
+    {
+
+    }
+
+    public function testEvents_insert()
+    {
+
+    }
+
+    public function testEvents_form()
+    {
+
+    }
+}
